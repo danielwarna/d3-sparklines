5e76aee3c25a102a869142f3deedfdbcc031d9f1
Daniel Wärnå <daniel@danielwarna.com>
2019-07-19 12:44:03 +0300

    Update demo view

--- src/demo.js
+++ src/demo.js
@@ -3,23 +3,36 @@
 import Sparklines from "./Sparklines.js";
 
 const data = [
-    5, 30, 25, 20, 50,75,80,100 
+    5, 30, 25, 20, 50,75,80,100
 ];
 
-var spark = new Sparklines("#elm", data, {
+var spark_bar = new Sparklines("#bar", data, {
     renderer:"bar",
     height:48
 });
 
-var line = new Sparklines("#line", data, {
+var spark_line = new Sparklines("#line", data, {
     renderer:"line",
-    height:48
+    height:68
 });
 
-var line = new Sparklines("#inline-line", data, {
+var spark_inline_bar = new Sparklines("#bar-inline", data, {
+    renderer:"bar",
+});
+
+var spark_inline_line = new Sparklines("#line-inline", data, {
+    renderer:"line",
+});
+
+var line_custom = new Sparklines("#line-customheight", data, {
+    renderer:"line",
+    height:28
+});
+
+var line_inline = new Sparklines("#inline-line", data, {
     renderer:"line"
 });
 
-var line = new Sparklines("#inline-line2", data, {
+var line_inline2 = new Sparklines("#inline-line2", data, {
     renderer:"line"
 });
