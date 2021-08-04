0029c5d1c633ce111dafee10b591c410d6214e0b
Daniel Wärnå <daniel@danielwarna.com>
2021-08-04 17:05:39 +0300

    Fix indentation

--- webpack.config.js
+++ webpack.config.js
@@ -3,22 +3,22 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: './index.html',
-  filename: 'index.html',
-  inject: 'body',
+    template: './index.html',
+    filename: 'index.html',
+    inject: 'body',
 });
 
 module.exports = {
-  entry: './src/index.js',
-  devtool: 'source-map',
-  output: {
-    path: path.resolve('dist'),
-    filename: 'index_bundle.js',
-  },
-  module: {
-    loaders: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-    ],
-  },
-  plugins: [HtmlWebpackPluginConfig],
+    entry: './src/index.js',
+    devtool: 'source-map',
+    output: {
+        path: path.resolve('dist'),
+        filename: 'index_bundle.js',
+    },
+    module: {
+        loaders: [
+            { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+        ],
+    },
+    plugins: [HtmlWebpackPluginConfig],
 };
